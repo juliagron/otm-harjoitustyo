70eba99d6bd6659c49cde4b005741f70c3d8fcd0
Julia Grön <julia.gron@helsinki.fi>
2018-03-25 22:35:24 +0300

    MaksukorttiTest

--- laskarit/viikko2/Unicafe/src/test/java/com/mycompany/unicafe/MaksukorttiTest.java
+++ laskarit/viikko2/Unicafe/src/test/java/com/mycompany/unicafe/MaksukorttiTest.java
@@ -17,4 +17,38 @@ public class MaksukorttiTest {
     public void luotuKorttiOlemassa() {
         assertTrue(kortti!=null);      
     }
+    
+    @Test
+    public void konstruktoriAsettaaSaldonOikein() {
+        assertEquals("saldo: 0.10", kortti.toString());
+    }
+    
+    @Test
+    public void lataaminenToimiiOikein() {
+        kortti.lataaRahaa(100);
+        assertEquals("saldo: 1.10", kortti.toString());
+    }
+    
+    @Test
+    public void rahanOttaminenToimiiOikein() {
+        kortti.lataaRahaa(200);
+        kortti.otaRahaa(10);
+        assertEquals("saldo: 2.0", kortti.toString());
+    }
+    
+    @Test
+    public void otaRahaaEiVieSaldoaNegatiiviseksi() {
+        kortti.otaRahaa(20);
+        assertEquals("saldo: 0.10", kortti.toString());
+    }
+    
+    @Test
+    public void rahaEiRiitaOtaMetodissa() {
+        assertFalse(kortti.otaRahaa(20));
+    }
+    
+    @Test
+    public void rahaRiittaaOtaMetodissa() {
+        assertTrue(kortti.otaRahaa(5));
+    }
 }
