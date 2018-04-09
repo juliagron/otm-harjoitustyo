8fbee4d20a16d818f8192a9129b7ff34f4eb86e9
Julia Grön <julia.gron@helsinki.fi>
2018-04-09 22:19:04 +0300

    kortti

--- Pasianssi/src/main/java/pasianssi/domain/Card.java
+++ Pasianssi/src/main/java/pasianssi/domain/Card.java
@@ -10,5 +10,37 @@ package pasianssi.domain;
  * @author juliagro
  */
 public class Card {
+    private Value value;
+    private Color color;
     
+    public Card(Value value, Color color){
+        this.value = value;
+        this.color = color;
+    }
+    
+    public Color getColor(){
+        return color;
+    }
+    
+    public Value getValue(){
+        return value;
+    }
+    
+    public enum Value{
+        ACE("A"), TWO("2"), THREE("3"), FOUR("4"),FIVE("5"),SIX("6"),SEVEN("7"), EIGHT("8"),NINE("9"), TEN("10"),JACK("J"),QUEEN("Q"),KING("K");
+        
+        public String str;
+        private Value(String str){
+            this.str = str;
+        }
+    }
+    
+    public enum Color{
+        CLUBS("risti"), SPADES("pata"),HEART("hertta"),DIAMONDS("ruutu");
+        
+        public String str;
+        private Color(String str){
+            this.str = str;
+        }
+    }
 }
