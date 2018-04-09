651060f313a872b064860ec3b7564c3b842e4d5a
Julia Grön <julia.gron@helsinki.fi>
2018-04-09 22:15:03 +0300

    kortti

---
+++ Pasianssi/src/main/java/pasianssi/ui/PasianssiUi.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pasianssi.ui;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+
+/**
+ *
+ * @author juliagro
+ */
+public class PasianssiUi extends Application {
+    
+    public static void main(String[] args){
+        launch(args);
+    }
+    
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        primaryStage.setTitle("Ikkuna");
+        
+        StackPane layout = new StackPane();
+        
+        Scene scene = new Scene(layout, 850, 600);
+        primaryStage.setScene(scene);
+        
+        primaryStage.show();
+        
+    }
+    
+}
