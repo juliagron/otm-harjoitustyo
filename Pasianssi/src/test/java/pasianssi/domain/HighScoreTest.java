4e280909490f498464d53e5d2edabac1e549f133
Julia Grön <julia.gron@helsinki.fi>
2018-04-10 19:43:47 +0300

    testejä

---
+++ Pasianssi/src/test/java/pasianssi/domain/HighScoreTest.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pasianssi.domain;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author juliagro
+ */
+public class HighScoreTest {
+    
+    public HighScoreTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    // @Test
+    // public void hello() {}
+    
+    @Test
+    public void constructorSetsCorrectValues() {
+        int id = 1;
+        String name = "jack";
+        String time = "2:45";
+        HighScore score = new HighScore(id, name, time);
+        assertTrue(score.getId() == id);
+        assertEquals(name, score.getName());
+        assertEquals(time, score.getTime());
+    }
+    
+}
