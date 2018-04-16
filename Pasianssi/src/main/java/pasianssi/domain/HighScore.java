631f48fbb7d36e71889dd7519ab6495be39df25a
Julia Grön <julia.gron@helsinki.fi>
2018-04-16 12:35:07 +0300

    päivitys

--- Pasianssi/src/main/java/pasianssi/domain/HighScore.java
+++ Pasianssi/src/main/java/pasianssi/domain/HighScore.java
@@ -13,12 +13,10 @@ public class HighScore {
     
     private String name;
     private String time;
-    private Integer id;
     
-    public HighScore(Integer id, String name, String time) {
+    public HighScore(String name, String time) {
         this.name = name;
         this.time = time;
-        this.id = id;
     }
     
     public String getName() {
@@ -29,19 +27,11 @@ public class HighScore {
         return this.time;
     }
     
-    public Integer getId() {
-        return this.id;
-    }
-    
     public void setName(String name) {
         this.name = name;
     }
     
     public void setTime(String time) {
         this.time = time;
-    }
-    
-    public void setId(Integer id) {
-        this.id = id;
     }
 }
