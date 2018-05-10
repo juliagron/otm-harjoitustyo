c0ddb0d711e0528b55c8570b4c7a3174bfc753b2
Julia Grön <julia.gron@helsinki.fi>
2018-05-10 23:39:22 +0300

    testejä

--- Pasianssi/src/test/java/pasianssi/domain/StartingSituationTest.java
+++ Pasianssi/src/test/java/pasianssi/domain/StartingSituationTest.java
@@ -55,18 +55,18 @@ public class StartingSituationTest {
     @Before
     public void setUp() {
         deck = new CardStack(0, 0);
-        waste = new CardStack(0, 1);
-        endstack1 = new CardStack(0, 3);
-        endstack2 = new CardStack(0, 4);
-        endstack3 = new CardStack(0, 5);
-        endstack4 = new CardStack(0, 6);
-        table1 = new CardStack(1, 0);
+        waste = new CardStack(1, 0);
+        endstack1 = new CardStack(3, 0);
+        endstack2 = new CardStack(4, 0);
+        endstack3 = new CardStack(5, 0);
+        endstack4 = new CardStack(6, 0);
+        table1 = new CardStack(0, 1);
         table2 = new CardStack(1, 1);
-        table3 = new CardStack(1, 2);
-        table4 = new CardStack(1, 3);
-        table5 = new CardStack(1, 4);
-        table6 = new CardStack(1, 5);
-        table7 = new CardStack(1, 6);
+        table3 = new CardStack(2, 1);
+        table4 = new CardStack(3, 1);
+        table5 = new CardStack(4, 1);
+        table6 = new CardStack(5, 1);
+        table7 = new CardStack(6, 1);
         situation = new StartingSituation(deck, waste, endstack1, endstack2, endstack3, endstack4, table1, table2, table3, table4, table5, table6, table7);
         cards = situation.getListOfAllCards();
         stacks = situation.getListOfAllStacks();
@@ -81,12 +81,64 @@ public class StartingSituationTest {
     //
     // @Test
     // public void hello() {}
-    /*
     @Test
     public void gettingListOfCards() {
         assertEquals(52, cards.size());
     }
 
+    @Test
+    public void gettingListOfAllTheStacks() {
+        assertEquals(13, stacks.size());
+    }
+
+    @Test
+    public void turningTopCardFaceUp1() {
+        situation.newDeal();
+        boolean faceup = true;
+        for (CardStack stack : stacks) {
+            if (stack.isTheStackOnTheTable()) {
+                if (!stack.topCard().isTheCardFaceUp()) {
+                    faceup = false;
+                }
+            }
+        }
+        assertTrue(faceup);
+    }
+
+    @Test
+    public void turningTheCardFaceUp2() {
+        situation.newDeal();
+        boolean faceup = false;
+        for (CardStack stack : stacks) {
+            for (int i = 0; i < stack.sizeOfTheStack() - 2; i++) {
+                faceup = stack.cards().get(i).isTheCardFaceUp();
+            }
+        }
+        assertFalse(faceup);
+    }
+    
+    @Test
+    public void correctNumberOfCardsOnTheStack() {
+        situation.newDeal();
+        boolean controlValue = true;
+        for (CardStack stack: stacks) {
+            if (stack.isTheStackNextToTheDeck() || stack.isTheStackOneOfTheEndStacks()) {
+                if (stack.sizeOfTheStack() != 0) {
+                    controlValue = false;
+                }
+            } else if (stack.isTheStackTheDeck()) {
+                if (stack.sizeOfTheStack() != 24) {
+                    controlValue = false;
+                }
+            } else if (stack.isTheStackOnTheTable()) {
+                if (stack.getLocationX() + 1 != stack.sizeOfTheStack()) {
+                    controlValue = false;
+                }
+            }
+        }
+        assertTrue(controlValue);
+    }
+    
     @Test
     public void sameDealDoesNotShuffleTheDeck() {
         List<Card> temp = new ArrayList();
@@ -94,7 +146,7 @@ public class StartingSituationTest {
         situation.sameDeal();
         assertEquals(temp, cards);
     }
-    
+
     @Test
     public void newDealShufflesTheDeck() {
         List<Card> temp = new ArrayList();
@@ -102,5 +154,5 @@ public class StartingSituationTest {
         situation.newDeal();
         assertThat(temp, IsNot.not(IsEqual.equalTo(cards)));
     }
-*/
+
 }
